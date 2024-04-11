64516e4c753dd6f5b446c8f4f7468e6e01142099
gilicohen12 <gili8cohen@gmail.com>
2024-04-11 09:37:47 -0400

    hw6 routes

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -2,7 +2,7 @@
 # Sample customers blueprint of endpoints
 # Remove this file if you are not using it in your project
 ########################################################
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
@@ -13,7 +13,7 @@ customers = Blueprint('customers', __name__)
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
+    cursor.execute('select id, company, last_name,\
         first_name, job_title, business_phone from customers')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -39,3 +39,27 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# make a customer update route
+@customers.route('/customers', methods=['PUT'])
+def put_customer():
+    data = request.json
+    current_app.logger.info(data)
+
+    cust_id = data['id']
+    company = data['company']
+    last_name = data['last_name']
+    first_name = data['first_name']
+    job_title = data['job_title']
+    business_phone = data['business_phone']
+
+    query = 'UPDATE customers SET company = %s, last_name = %s, first_name = %s, job_title = %s, business_phone = %s WHERE id = %s'
+    data= (company, last_name, first_name, job_title, business_phone, cust_id)
+    cursor = db.get_db().cursor()
+    r = cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Customer updated!'
+
+
+
+   
