013794be3065d10507c6478fe1245f4ce5a48037
eavonkirchbach <vonkirchbach.e@northeastern.edu>
2024-04-12 14:05:47 -0400

    create meals

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -37,6 +37,7 @@ def create_app():
     from src.products.products  import products
     from src.recipies  import recipies
     from src.ingredients  import ingredients
+    from src.meals  import meals
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
@@ -44,6 +45,7 @@ def create_app():
     app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(recipies,    url_prefix='/r')
     app.register_blueprint(ingredients,    url_prefix='/i')
+    app.register_blueprint(meals,    url_prefix='/m')
 
     # Don't forget to return the app object
     return app
---
+++ flask-app/src/meals.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from src import db
